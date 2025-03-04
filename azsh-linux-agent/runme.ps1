743c739bf02b719a46ab1fc18072238a8515780d
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-04 00:28:28 -0500

    can push to azure container registry

--- azsh-linux-agent/runme.ps1
+++ azsh-linux-agent/runme.ps1
@@ -1,6 +1,11 @@
 $your_tag = "yourtag"
+$containerRegistryName = "cragentssgvhe4aipy37o.azurecr.io"
 
-docker build --tag "azsh-linux-agent:${your_tag}" --file "./azsh-linux-agent.dockerfile" .
+docker build --tag "azsh-linux-agent:${your_tag}" `
+    --tag "${containerRegistryName}/azsh-linux-agent:${your_tag}" `
+    --tag "${containerRegistryName}/azsh-linux-agent:latest" `
+    --file "./azsh-linux-agent.dockerfile" .
 
 # Push to your registry in azure
-docker tag
+docker push "${containerRegistryName}/azsh-linux-agent:${your_tag}"
+docker push "${containerRegistryName}/azsh-linux-agent:latest"
