b688a059c16768042a64a54d2064f0d6a24d4b12
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-04 00:59:06 -0500

    ok

--- azsh-linux-agent/02-deploy-to-k8s.ps1
+++ azsh-linux-agent/02-deploy-to-k8s.ps1
@@ -1,1 +1,9 @@
-azsh-linux-agent-secret.yaml 
+$yamlFile = "azsh-linux-agent-secret.yaml"
+$yamlFileTemplate = "azsh-linux-agent-secret.template.yaml"
+
+# Replace the placeholder with the actual value
+$template = Get-Content $yamlFileTemplate
+$template = $template -replace "__PAT_TOKEN__", $env:AZURE_DEVOPS_EXT_PAT_SOURCE
+
+# Write the updated content to the yaml file
+$template | Set-Content $yamlFile
