643920bccdcf67207681479e3994e7709409d04a
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-04 20:33:48 -0500

    bionic dind builds

---
+++ dind/01-Install-DinD.ps1
@@ -0,0 +1,19 @@
+#https://medium.com/@muppedaanvesh/azure-devops-self-hosted-agents-on-kubernetes-part-3-6658d741b369
+
+#$kubeContext = "my-workload-cluster-007-admin@my-workload-cluster-007"
+$your_tag = "ubuntu-18.04"
+$containerRegistryName = "cragentssgvhe4aipy37o.azurecr.io"
+$repositoryName = "linux-sh-agent-dind"
+$dockerFileName = "./Dockerfile"
+
+docker build --tag "${repositoryName}:${your_tag}" `
+    --tag "${containerRegistryName}/${repositoryName}:${your_tag}" `
+    --tag "${containerRegistryName}/${repositoryName}:latest" `
+    --file "$dockerFileName" .
+
+# login to azure container registry
+az acr login --name $containerRegistryName
+
+# Push to your registry in azure
+docker push "${containerRegistryName}/${repositoryName}:${your_tag}"
+docker push "${containerRegistryName}/${repositoryName}:latest"
