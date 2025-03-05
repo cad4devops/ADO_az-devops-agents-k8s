0ea2f325e8d6172c1412e1b116afbaca494b7d89
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-05 17:24:33 -0500

    test also windows

---
+++ keda/windows/01-Install-Keda.ps1
@@ -0,0 +1,20 @@
+#https://medium.com/@muppedaanvesh/azure-devops-self-hosted-agents-on-kubernetes-part-3-6658d741b369
+
+$kubeContext = "my-workload-cluster-007-admin@my-workload-cluster-007"
+$namespaceKeda = "keda"
+$namespacePrometheus = "prometheus-stack"
+
+kubectl config use-context $kubeContext
+
+kubectl get nodes -o wide
+
+helm repo add kedacore https://kedacore.github.io/charts
+
+helm repo update
+
+helm install keda kedacore/keda --namespace $namespaceKeda --create-namespace
+
+kubectl get all -n $namespaceKeda
+
+# Get the Prometheus server URL by running these commands in the same shell:
+kubectl port-forward -n $namespacePrometheus service/prometheus-stack-grafana 3000:80
