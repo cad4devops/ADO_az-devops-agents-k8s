5daafcf7188b40e2397316093252473d5aeb9436
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-06 13:32:24 -0500

    TODO fix tolerations

--- keda/windows/02-Install-Keda.ps1
+++ keda/windows/02-Install-Keda.ps1
@@ -1,6 +1,6 @@
 #https://medium.com/@muppedaanvesh/azure-devops-self-hosted-agents-on-kubernetes-part-3-6658d741b369
 
-$kubeContext = "my-workload-cluster-007-admin@my-workload-cluster-007"
+$kubeContext = "my-workload-cluster-008-admin@my-workload-cluster-008"
 $namespaceKeda = "keda"
 $namespacePrometheus = "prometheus-stack"
 $yamlSecretFileTemplate = "keda-secret.template.yaml"
