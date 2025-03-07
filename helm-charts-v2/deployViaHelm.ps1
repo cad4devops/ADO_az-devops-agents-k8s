1392b96396537456670ca7ddddd054484c6f5594
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-07 10:05:26 -0500

    add a script for helm

---
+++ helm-charts-v2/deployViaHelm.ps1
@@ -0,0 +1,10 @@
+# create namespaces if not exists
+$linuxNamespace = "az-devops-linux"
+$windowsNamespace = "az-devops-windows"
+
+kubectl create namespace $linuxNamespace --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace $windowsNamespace --dry-run=client -o yaml | kubectl apply -f -
+
+helm install az-selfhosted-agents ./az-selfhosted-agents `
+    --set windows.enabled=true `
+    --set linux.enabled=true
