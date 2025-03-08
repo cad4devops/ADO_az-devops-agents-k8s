5af6653d053f115cfd1b81d580d3befc6c871242
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-08 18:01:54 -0500

    convert to base64 for encodings

--- helm-charts-v2/deployViaHelm.ps1
+++ helm-charts-v2/deployViaHelm.ps1
@@ -10,11 +10,26 @@ param (
     $linuxNamespace = "az-devops-linux-$instanceNumber",
     [Parameter()]
     [string]
-    $windowsNamespace = "az-devops-windows-$instanceNumber"
+    $windowsNamespace = "az-devops-windows-$instanceNumber",
+    [Parameter()]
+    [string]
+    $linuxPoolName = "KubernetesPoolLinux",
+    [Parameter()]
+    [string]        
+    $windowsPoolName = "KubernetesPoolWindows"
 )
 
 Write-Output "Linux Namespace: $linuxNamespace"
 Write-Output "Windows Namespace: $windowsNamespace"
+Write-Output "Linux Pool Name: $linuxPoolName"
+Write-Output "Windows Pool Name: $windowsPoolName"
+
+# convert the pool name to base64
+$linuxPoolNameBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($linuxPoolName))
+$windowsPoolNameBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($windowsPoolName))
+
+Write-Output "Linux Pool Name Base64: $linuxPoolNameBase64"
+Write-Output "Windows Pool Name Base64: $windowsPoolNameBase64"
 
 kubectl create namespace $linuxNamespace --dry-run=client -o yaml | kubectl apply -f -
 kubectl create namespace $windowsNamespace --dry-run=client -o yaml | kubectl apply -f -
