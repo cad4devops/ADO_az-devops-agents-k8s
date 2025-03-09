467403533091b88e43b8323dd18cd5ef7d27ae82
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-09 08:23:11 -0400

    slowly injecting values

--- helm-charts-v2/deployViaHelm.ps1
+++ helm-charts-v2/deployViaHelm.ps1
@@ -16,8 +16,24 @@ param (
     $linuxPoolName = "KubernetesPoolLinux",
     [Parameter()]
     [string]        
-    $windowsPoolName = "KubernetesPoolWindows"
+    $windowsPoolName = "KubernetesPoolWindows",
+    [Parameter()]
+    [string]
+    $organizationUrl = "https://dev.azure.com/cad4devops"
 )
+
+# get pat from environment variable
+$pat = $env:AZURE_DEVOPS_EXT_PAT
+if (-not $pat) {
+    Write-Error "Please set the AZURE_DEVOPS_EXT_PAT environment variable."
+    exit 1
+}
+
+# ech parameters
+Write-Output "Organization URL: $organizationUrl"
+Write-Output "Instance Number: $instanceNumber"
+# Set the Azure DevOps organization URL and personal access token (PAT)
+Write-Output "Using PAT: $($pat.Substring(0, 4))... (truncated for security)"
 
 Write-Output "Linux Namespace: $linuxNamespace"
 Write-Output "Windows Namespace: $windowsNamespace"
@@ -27,16 +43,27 @@ Write-Output "Windows Pool Name: $windowsPoolName"
 # convert the pool name to base64
 $linuxPoolNameBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($linuxPoolName))
 $windowsPoolNameBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($windowsPoolName))
+$organizationUrlBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($organizationUrl))
+$patBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($pat))
 
 Write-Output "Linux Pool Name Base64: $linuxPoolNameBase64"
 Write-Output "Windows Pool Name Base64: $windowsPoolNameBase64"
+Write-Output "Organization URL Base64: $organizationUrlBase64"
+# truncated for security
+Write-Output "PAT Base64: $($patBase64.Substring(0, 4))... (truncated for security)"
 
 kubectl create namespace $linuxNamespace --dry-run=client -o yaml | kubectl apply -f -
 kubectl create namespace $windowsNamespace --dry-run=client -o yaml | kubectl apply -f -
 
 helm install az-selfhosted-agents ./az-selfhosted-agents `
     --set windows.enabled=true `
+    --set secretwindows.data.AZP_TOKEN_VALUE=$patBase64 `
+    --set secretwindows.data.AZP_POOL_VALUE=$windowsPoolNameBase64 `
+    --set secretwindows.data.AZP_URL_VALUE=$organizationUrlBase64 `
     --set linux.enabled=true `
+    --set secretlinux.data.AZP_TOKEN_VALUE=$patBase64 `
+    --set secretlinux.data.AZP_POOL_VALUE=$linuxPoolNameBase64 `
+    --set secretlinux.data.AZP_URL_VALUE=$organizationUrlBase64 `
     --create-namespace `
     --namespace $linuxNamespace
 
